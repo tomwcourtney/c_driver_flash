45ad2e8df42607f3270f833f348abb6fb67e4b66
Thomas Courtney <77306308+tomwcourtney@users.noreply.github.com>
2023-09-05 10:15:07 +1000

    Added template files and readme.

---
+++ test-harness/tests/spy_test_.cpp
@@ -0,0 +1,25 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C" 
+{
+    #include "../spies/spy_.h"
+}
+
+
+TEST_GROUP(SpyTest)
+{
+    void setup()
+    {
+        
+    }
+
+    void teardown()
+    {
+    }
+};
+
+// Zero
+
+// One
+
+// Many
---
+++ test-harness/tests/test_.cpp
@@ -0,0 +1,31 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C" 
+{
+
+}
+
+
+TEST_GROUP(Test) 
+{
+    void setup()
+    {
+
+    }
+
+    void teardown()
+    {
+    }
+
+  
+
+};
+
+// Zero
+TEST(Test, example_test)
+{
+    CHECK(1);
+}
+// One
+
+// Many
