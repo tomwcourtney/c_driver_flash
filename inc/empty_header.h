d69405a4c8ec3dd957d5bef520a6c76b183db511
Thomas Courtney <77306308+tomwcourtney@users.noreply.github.com>
2023-09-14 06:53:30 +1000

    Add starter source files with sections commented.

---
+++ inc/empty_header.h
@@ -0,0 +1,19 @@
+#ifndef EMPTY_HEADER_H
+#define EMPTY_HEADER_H
+/***********************/
+/* PUBLIC DEFINITIONS */
+/***********************/
+
+/****************/
+/* PUBLIC TYPES */
+/****************/
+
+/********************/
+/* PUBLIC VARIABLES */
+/********************/
+
+/********************************/
+/* PUBLIC FUNCTION DECLARATIONS */
+/********************************/
+
+#endif
